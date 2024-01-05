33a4b83b532a5d415ce6ef132381cccd95a3c68f
Edouard CATTEZ <edouard.cattez@gmail.com>
2024-01-05 17:22:09 +0100

    feat: source retention on test dragees

--- annotation-processor/src/test/java/io/dragee/testing/KindTwo.java
+++ annotation-processor/src/test/java/io/dragee/testing/KindTwo.java
@@ -1,9 +1,12 @@
 package io.dragee.testing;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Testing
 @Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
 public @interface KindTwo {
 }
--- annotation-processor/src/test/java/io/dragee/testing/Testing.java
+++ annotation-processor/src/test/java/io/dragee/testing/Testing.java
@@ -4,7 +4,6 @@ import io.dragee.annotation.Dragee;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-
 @Dragee.Namespace
 @Target({ElementType.ANNOTATION_TYPE})
 public @interface Testing {
