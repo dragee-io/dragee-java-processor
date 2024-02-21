35dde52e20326c87b9bf5155eb4798213d4b18ed
Edouard CATTEZ <edouard.cattez@gmail.com>
2024-02-21 20:59:47 +0100

    feat: remove extension

--- annotation-processor/src/test/java/io/dragee/rules/dragee_extension/DrageeGrandParent.java
+++
@@ -1,9 +0,0 @@
-package io.dragee.rules.dragee_extension;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@ExtensionTest
-@Target({ElementType.TYPE})
-public @interface DrageeGrandParent {
-}
