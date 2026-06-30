adb187baa1046688b60f69a14e79b18e460008a7
Dhiaeddine Merad <113862087+Diozizo@users.noreply.github.com>
2026-06-30 11:06:27 +0200

    feat: add three-tier-annotations module (#25)
    * feat: add three-tier-annotations module
    
    * feat: add description to three-tier and hexagonal annotation poms
    
    * refactor: rename three-tier Presenter annotation to Controller
    
    * rename three-tier Presenter annotation to Controller

---
+++ three-tier-annotations/src/main/java/io/fixentropy/annotation/threetier/ThreeTier.java
@@ -0,0 +1,69 @@
+package io.fixentropy.annotation.threetier;
+
+import io.fixentropy.annotation.Fixentropy;
+
+import java.lang.annotation.*;
+
+@Fixentropy.Namespace
+@Inherited
+@Documented
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ThreeTier {
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Entity {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface RequestModel {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface ResponseModel {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Service {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Repository {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Controller {
+    }
+
+    @ThreeTier
+    @Documented
+    @Inherited
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Mapper {
+    }
+}
---
+++ three-tier-annotations/src/test/java/io/fixentropy/annotation/threetier/sample/AController.java
@@ -0,0 +1,7 @@
+package io.fixentropy.annotation.threetier.sample;
+
+import io.fixentropy.annotation.threetier.ThreeTier;
+
+@ThreeTier.Controller
+public class AController {
+}
